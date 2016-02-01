c2746132ba64f967d5e40f2a73fa7832d272e76f
freejacque <freejacque@gmail.com>
2016-01-31 19:42:32 -0500

    creatd basic structure for the footer partial

--- kick-punch-block/footer.php
+++ kick-punch-block/footer.php
@@ -1,7 +1,5 @@
-<div id="page" class="hfeed site">
-  <header id="masthead" class="site-header" role="banner">
-    <hgroup></hgroup>
-    <nav role="navigation" class="site-navigation main-navigation"></nav>
-  </header>
-</div>
-<div id="main" class="site-main">
+  </div>
+  <footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="site-info"></div>
+  </footer>
+</div><!-- #page .hfeed .site -->
