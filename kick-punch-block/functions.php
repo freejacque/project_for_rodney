24d5071be4516e7bd53419085d839068c2b5f5cf
freejacque <freejacque@gmail.com>
2016-01-31 20:46:18 -0500

    created functions file and set content_width

---
+++ kick-punch-block/functions.php
@@ -0,0 +1,14 @@
+<?
+/** KPB functions and definitions
+ *
+ * @package Kick-Punch-Block
+ * @since  Kick-Punch-Block 1.0
+ */
+
+/**
+ * Set the content width based on the theme's design and stylesheet.
+ * @since Kick-Punch-Block 1.0
+ */
+if(! isset( $content_width ) )
+  $content_width = 654; /* pixels */
+
