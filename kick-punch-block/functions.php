82ac61871480e12e38bb42bacfc7fd2ffbf327b4
freejacque <freejacque@gmail.com>
2016-02-01 20:59:11 -0500

    added comments to KPB setup

--- kick-punch-block/functions.php
+++ kick-punch-block/functions.php
@@ -14,7 +14,7 @@ if(! isset( $content_width ) )
   $content_width = 654; /* pixels */
 
 if( ! function_exists( 'kpb_setup' ) ):
-  /*
+  /* *
    * Sets up theme defaults and registers support for various wordpress features.
    *
    * Note: this function is hooked into the after_setup_theme hook
@@ -23,11 +23,15 @@ if( ! function_exists( 'kpb_setup' ) ):
    * @since Kick-Punch-Block 1.0
    */
   function kpb_setup() {
+    // Custom template tags
     require( get_template_directory() ) . '/inc/template-tags.php' );
+    // Custom independently acting functions
     require( get_template_directory() ) . '/inc/tweaks.php' );
-
+    //  make the theme available for translation
     load_theme_textdomain( 'Kick-Punch-Block', get_template_directory() . '/languages' );
+    //  add default posts & rss feed links to head
     add_theme_support( 'automatic-feed-links' );
+    // Enable support for the Aside Post Format
     add_theme_support( 'post-formats', array( 'aside' ) );
     register_nav_menus( array(
       'primary' => __( 'Primary Menu', 'Kick-Punch-Block' ),
