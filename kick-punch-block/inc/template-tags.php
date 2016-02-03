9d06f3993bd9060ed1a0f3bfd9e2cf97003eab55
freejacque <freejacque@gmail.com>
2016-02-02 22:07:51 -0500

    added basic doc for template tags file

--- kick-punch-block/inc/template-tags.php
+++ kick-punch-block/inc/template-tags.php
@@ -1,1 +1,8 @@
-<!-- template tags are WP functions used to display dynamic info -->
+<?php
+/*
+ * Custome template tags for KPB theme.
+ * some of the functionality here may be replaced by core functions
+ *
+ * @package Kick-Punch-Block
+ * @since  Kick-Punch-Block 1.0
+ */
