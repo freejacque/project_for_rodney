7ee5080c5b1f4a5f820cee91cb0954b4c262d24f
freejacque <freejacque@gmail.com>
2016-02-02 21:57:28 -0500

    added a comment

--- kick-punch-block/functions.php
+++ kick-punch-block/functions.php
@@ -38,5 +38,5 @@ if( ! function_exists( 'kpb_setup' ) ):
     ));
   }
 endif;
-// kpb_setup is being "hooked" into the function after_setup_theme
+// telling wordpres to run kpb_setup when it runs after_setup_theme
 add_action( 'after_setup_theme', 'kpb_setup' );
