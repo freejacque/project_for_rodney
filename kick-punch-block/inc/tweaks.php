e3c5c04c6487efd608189c53cc361da8118ef5f3
freejacque <freejacque@gmail.com>
2016-02-02 22:10:25 -0500

    added basic doc for tweaks file

--- kick-punch-block/inc/tweaks.php
+++ kick-punch-block/inc/tweaks.php
@@ -1,1 +1,9 @@
-tweaks.php
+<?php
+/**
+ * Custom functions that act independently of the theme templates
+ *
+ * Eventually, some of the functionality here could be replaced by core features
+ *
+ * @package Kick-Punch-Block
+ * @since  Kick-Punch-Block 1.0
+ */
