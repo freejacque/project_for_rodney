4a530a5b0703de4674345f2fe72a4d5ccae2d558
freejacque <freejacque@gmail.com>
2016-02-04 05:44:32 -0500

    altered tweaks

--- kick-punch-block/inc/tweaks.php
+++ kick-punch-block/inc/tweaks.php
@@ -1,9 +1,7 @@
 <?php
 /**
  * Custom functions that act independently of the theme templates
- *
  * Eventually, some of the functionality here could be replaced by core features
- *
  * @package Kick-Punch-Block
  * @since  Kick-Punch-Block 1.0
  */
@@ -11,22 +9,19 @@
 
 /**
  * Get the wp_nav_menu() fallback, wp_page_menu(), to show a home link.
- *
  * @since Kick-Punch-Block 1.0
  */
-
 function kpb_page_menu_args( $args ) {
   $args['show_home'] = true;
   return $args;
 }
 add_filter( 'wp_page_menu_args', 'kpb_page_menu_args' );
 
+
 /**
  * Adds custom classes to the array of body classes.
- *
  * @since Kick-Punch-Block 1.0
  */
-
 function kpb_body_classes( $classes ) {
   //  adds a class of group-blog to blogs with > 1 published author
   if( is_multi_author() ) {
@@ -37,9 +32,9 @@ function kpb_body_classes( $classes ) {
 }
 add_filter( 'body_class', 'kpb_body_classes' );
 
+
 /**
  * Filter in a link to a content ID attribute for the next/previous image links on image attachment pages
- *
  * @since Kick-Punch-Block 1.0
  */
  function kpb_enhanced_image_navigation( $url, $id ) {
