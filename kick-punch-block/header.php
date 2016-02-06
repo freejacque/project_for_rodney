29c30f480b927acb89d9e5bddd7d277ac69b8b53
freejacque <freejacque@gmail.com>
2016-02-05 21:45:39 -0500

    added doctype and comments to header partial

--- kick-punch-block/header.php
+++ kick-punch-block/header.php
@@ -1,3 +1,14 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package Kick-Punch-Block
+ * @since Kick-Punch-Block 1.0
+ */
+?><!DOCTYPE html>
+
 <div id="page" class="hfeed site">
   <header id="masthead" class="site-header" role="banner">
     <hgroup></hgroup>
