25bd823ddd8b129b1aa840411dd1f5d7350eb8be
freejacque <freejacque@gmail.com>
2016-02-06 19:11:09 -0500

    added opening body tag

--- kick-punch-block/header.php
+++ kick-punch-block/header.php
@@ -36,13 +36,20 @@
   echo ' | '. sprintf( __( 'Page %s', 'Kick-Punch-Block'), max( $paged, $page ) );
 
   ?></title>
-
+  <link rel="profile" href="http://gmpg.org/xfn/11" />
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+  <!--[if lt IE 9]>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+  <![endif]-->
+  <?php wp_head(); ?>
 </head>
 
-<div id="page" class="hfeed site">
-  <header id="masthead" class="site-header" role="banner">
-    <hgroup></hgroup>
-    <nav role="navigation" class="site-navigation main-navigation"></nav>
-  </header>
-</div>
-<div id="main" class="site-main">
+<body <?php body_class(); ?>>
+
+  <div id="page" class="hfeed site">
+    <header id="masthead" class="site-header" role="banner">
+      <hgroup></hgroup>
+      <nav role="navigation" class="site-navigation main-navigation"></nav>
+    </header>
+  </div>
+  <div id="main" class="site-main">
