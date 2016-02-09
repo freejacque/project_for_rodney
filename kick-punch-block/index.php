7ef30db307f3f6383c5bb6b13e87b0e9bdd725ff
freejacque <freejacque@gmail.com>
2016-02-08 20:22:08 -0500

    added a comment

--- kick-punch-block/index.php
+++ kick-punch-block/index.php
@@ -1,4 +1,15 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The main template file.
+ *
+ * This is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Kick-Punch-Block
+ * @since Kick-Punch-Block 1.0
+ */
+get_header(); ?>
 
 <div id="primary" class="content-area">
   <div id="content" class="site-content" role="main">
