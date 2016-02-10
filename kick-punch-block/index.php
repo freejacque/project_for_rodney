375f9d842fd00aec2680e4fd66a9cab07e423d96
freejacque <freejacque@gmail.com>
2016-02-09 20:23:28 -0500

    added the loop to index.php

--- kick-punch-block/index.php
+++ kick-punch-block/index.php
@@ -11,6 +11,10 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
   <div id="content" class="site-content" role="main">
+    <?php /* Start the loop */ ?>
+    <?php while( have_posts()) : the_post(); ?>
+      <?php the_content(); ?>
+    <?php endwhile; ?>
   </div>
 </div>
 
