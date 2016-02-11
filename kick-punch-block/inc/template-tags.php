2a099accbb5410331eed5f655c62f4a8237cd8eb
freejacque <freejacque@gmail.com>
2016-02-10 21:44:14 -0500

    started a conditional in template-tags

--- kick-punch-block/inc/template-tags.php
+++ kick-punch-block/inc/template-tags.php
@@ -6,3 +6,14 @@
  * @package Kick-Punch-Block
  * @since  Kick-Punch-Block 1.0
  */
+
+if( ! function_exists( 'kpb_posted_on' ) ) :
+/**
+ * Prints HTML with meta information for the current post-date/time and author.
+ *
+ * @since Kick-Punch-Block 1.0
+ */
+function kpb_posted_on() {
+
+}
+endif;
