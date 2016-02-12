c1e47ec0f9a269ad03f03191212e4e1dec741cc0
freejacque <freejacque@gmail.com>
2016-02-11 21:43:43 -0500

    completed kpb_posted_on

--- kick-punch-block/inc/template-tags.php
+++ kick-punch-block/inc/template-tags.php
@@ -14,6 +14,14 @@ if( ! function_exists( 'kpb_posted_on' ) ) :
  * @since Kick-Punch-Block 1.0
  */
 function kpb_posted_on() {
-
+  printf( __( 'Posted on <a href="%1$" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a><span class="byline"> by <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'kpb' ),
+    esc_url( get_permalink() ),
+    esc_attr( get_the_time() ),
+    esc_attr( get_the_date( 'c' ) ),
+    esc_html( get_the_date() ),
+    esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+    esc_attr( sprintf( __( 'View all posts by %s', 'kpb' ), get_the_author() ) ),
+    esc_html( get_the_author() )
+  );
 }
 endif;
