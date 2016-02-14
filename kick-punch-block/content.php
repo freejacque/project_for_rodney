ce439b7993ecf9ecfbe595f989d863f19ac28e64
freejacque <freejacque@gmail.com>
2016-02-14 18:41:00 -0500

    added loop code partial to content.php

--- kick-punch-block/content.php
+++ kick-punch-block/content.php
@@ -17,3 +17,37 @@
     </div>
     <?php endif; ?>
   </header>
+
+  <?php if( is_search() ) : ?>
+  <div class="entry-summary">
+    <?php the_excerpt(); ?>
+  </div>
+  <?php else : ?>
+  <div class="entry-content">
+    <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'kpb' ) ); ?>
+    <?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'kpb' ), 'after' => '</div>' ) ); ?>
+  </div>
+  <?php endif; ?>
+
+  <footer class="entry-meta">
+    <?php if( 'post' == get_post_type() ) : ?>
+      <?php $categories_list = get_the_category_list( __( ', ', 'kpb' ) ); if( $categories_list && kpb_categorized_blog() ) : ?>
+      <span class="cat-links">
+        <?php printf( __( 'Posted in %1$s', 'kpb' ), $categories_list ); ?>
+      </span>
+      <?php $tags_list = get_the_tag_list( '', __( ',', 'kpb' ) ); if( $tags_list ) : ?>
+      <span class="sep"> | </span>
+      <span class="tag-links">
+        <?php printf( __( 'Tagged %1$s', 'kpb' ), $tags_list ); ?>
+      </span>
+      <?php endif; ?>
+    <?php endif; ?>
+
+    <?php if( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+    <span class="sep"> | </span>
+    <span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'kpb' ), __( '1 Comment', 'kpb' ), __( '% Comments', 'kpb' ) ); ?></span>
+    <?php endif; ?>
+
+    <?php edit_post_link( __( 'Edit', 'kpb' ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
+  </footer>
+</article>
