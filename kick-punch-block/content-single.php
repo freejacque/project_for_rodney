ed3e6d4a302fee7c29eb946abd9b852debc15b53
freejacque <freejacque@gmail.com>
2016-02-17 20:30:13 -0500

    created content-single.php template

---
+++ kick-punch-block/content-single.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @package Kick-Punch-Block
+ * @since Kick-Punch-Block 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header">
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+
+        <div class="entry-meta">
+            <?php shape_posted_on(); ?>
+        </div>
+    </header>
+
+    <div class="entry-content">
+        <?php the_content(); ?>
+        <?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'kpb' ), 'after' => '</div>' ) ); ?>
+    </div>
+
+    <footer class="entry-meta">
+        <?php
+
+            $category_list = get_the_category_list( __( ', ', 'kpb' ) );
+
+                        $tag_list = get_the_tag_list( '', __( ', ', 'kpb' ) );
+
+            if ( ! shape_categorized_blog() ) {
+
+                if ( '' != $tag_list ) {
+                    $meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'kpb' );
+                } else {
+                    $meta_text = __( 'Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'kpb' );
+                }
+
+            } else {
+
+                if ( '' != $tag_list ) {
+                    $meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'kpb' );
+                } else {
+                    $meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'kpb' );
+                }
+
+            }
+
+            printf(
+                $meta_text,
+                $category_list,
+                $tag_list,
+                get_permalink(),
+                the_title_attribute( 'echo=0' )
+            );
+        ?>
+
+        <?php edit_post_link( __( 'Edit', 'kpb' ), '<span class="edit-link">', '</span>' ); ?>
+    </footer>
+</article>
