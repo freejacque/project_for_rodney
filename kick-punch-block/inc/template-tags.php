2698e1af7fb7ad26422e41ebd8ceddd33232395a
freejacque <freejacque@gmail.com>
2016-02-19 05:52:08 -0500

    created kpb_comment

--- kick-punch-block/inc/template-tags.php
+++ kick-punch-block/inc/template-tags.php
@@ -110,3 +110,59 @@ function kpb_content_nav( $nav_id ) {
   <?php
 }
 endif;
+
+if ( ! function_exists( 'kpb_comment' ) ) :
+/**
+ * Template for comments and pingbacks.
+ *
+ * Used as a callback by wp_list_comments() for displaying the comments.
+ *
+ * @since Kick-Punch-Block 1.0
+ */
+function kpb_comment( $comment, $args, $depth ) {
+  $GLOBALS['comment'] = $comment;
+  switch ( $comment->comment_type ) :
+    case 'pingback' :
+    case 'trackback' :
+  ?>
+  <li class="post pingback">
+    <p><?php _e( 'Pingback:', 'kpb' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __( '(Edit)', 'kpb' ), ' ' ); ?></p>
+  <?php
+    break;
+    default :
+  ?>
+  <li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
+    <article id="comment-<?php comment_ID(); ?>" class="comment">
+      <footer>
+        <div class="comment-author vcard">
+          <?php echo get_avatar( $comment, 40 ); ?>
+          <?php printf( __( '%s <span class="says">says:</span>', 'kpb' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
+        </div>
+        <?php if ( $comment->comment_approved == '0' ) : ?>
+          <em><?php _e( 'Your comment is awaiting moderation.', 'kpb' ); ?></em>
+          <br />
+        <?php endif; ?>
+
+        <div class="comment-meta commentmetadata">
+          <a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><time pubdate datetime="<?php comment_time( 'c' ); ?>">
+          <?php
+
+              printf( __( '%1$s at %2$s', 'kpb' ), get_comment_date(), get_comment_time() ); ?>
+          </time></a>
+          <?php edit_comment_link( __( '(Edit)', 'kpb' ), ' ' );
+          ?>
+        </div>
+      </footer>
+
+      <div class="comment-content"><?php comment_text(); ?></div>
+
+      <div class="reply">
+        <?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+      </div>
+    </article>
+
+  <?php
+          break;
+  endswitch;
+}
+endif;
