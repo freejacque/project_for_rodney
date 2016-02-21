f61f1ec3f00fa4381473370fa1d7cb4e60f8122f
freejacque <freejacque@gmail.com>
2016-02-20 19:21:55 -0500

    added a comment

--- kick-punch-block/comments.php
+++ kick-punch-block/comments.php
@@ -13,7 +13,11 @@
 ?>
 
 <?php
-
+    /*
+     * If the current post is protected by a password and
+     * the visitor has not yet entered the password
+     * return early without loading the comments.
+     */
     if ( post_password_required() )
         return;
 ?>
