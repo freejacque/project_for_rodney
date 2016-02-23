ee09e233bbf0412fdf32565dd5c7b676f81235a2
freejacque <freejacque@gmail.com>
2016-02-22 21:00:28 -0500

    added a comment

--- kick-punch-block/archive.php
+++ kick-punch-block/archive.php
@@ -26,7 +26,7 @@ get_header(); ?>
                 //Queue the first post
                 the_post();
                 printf( __( 'Author Archives: %s', 'kpb' ), '<span class="vcard"><a class="url fn n" href="' . get_author_posts_url( get_the_author_meta( "ID" ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' );
-
+                // go back to the beginning of the loop
                 rewind_posts();
 
             } elseif ( is_day() ) {
