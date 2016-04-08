5961eca4c1fd61f2367b8578207d931f8bb09aef
freejacque <freejacque@gmail.com>
2016-04-07 21:02:04 -0400

    started pow template

---
+++ kick-punch-block/photo-of-week.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template for displaying posts in the Aside post format
+ * @package Kick-Punch-Block
+ * @since Kick-Punch-Block 1.0
+ */
+?>
+
+
+<div id="photo-of-the-week-container">
+  <h1 class="pow-title">VS PHOTO OF THE WEEK</h1>
+  <p class="">Tag your entry photo with <span id="hashTag">#VSbestpic</span> on Instagram or Twitter and win a chance to be featured here and our headquarters for an entire week!</p>
+  <p class="">This week's winner: <span id="winnerName"></span></p>
+  <div id="pow-photo">
+    <img src="" alt="">
+  </div>
+</div>
--- kick-punch-block/searchform.php
+++ kick-punch-block/searchform.php
@@ -6,8 +6,8 @@
  * @since Kick-Punch-Block 1.0
  */
 ?>
-    <form method="get" id="searchForm" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-        <label for="s" class="assistive-text"><?php _e( 'Search', 'kpb' ); ?></label>
-        <input type="text" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', 'kpb' ); ?>" />
-        <input type="submit" class="submit" name="submit" id="searchSubmit" value="<?php esc_attr_e( 'Search', 'kpb' ); ?>" />
-    </form>
+  <form method="get" id="searchForm" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+    <label for="s" class="assistive-text"><?php _e( 'Search', 'kpb' ); ?></label>
+    <input type="text" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', 'kpb' ); ?>" />
+    <input type="submit" class="submit" name="submit" id="searchSubmit" value="<?php esc_attr_e( 'Search', 'kpb' ); ?>" />
+  </form>
