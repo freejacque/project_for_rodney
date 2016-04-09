de0a0c606d2a06c81a3be511ac19000a22d87e09
freejacque <freejacque@gmail.com>
2016-04-08 21:14:17 -0400

    added php tag

--- kick-punch-block/photo-of-week.php
+++ kick-punch-block/photo-of-week.php
@@ -10,7 +10,7 @@
 <div id="photo-of-the-week-container">
   <h1 class="pow-title">VS PHOTO OF THE WEEK</h1>
   <p class="">Tag your entry photo with <span id="hashTag">#VSbestpic</span> on Instagram or Twitter and win a chance to be featured here and our headquarters for an entire week!</p>
-  <p class="">This week's winner: <span id="winnerName"></span></p>
+  <p class="">This week's winner: <span id="winnerName"><?php ?></span></p>
   <div id="pow-photo">
     <img src="" alt="">
   </div>
