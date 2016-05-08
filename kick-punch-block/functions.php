0917cf6a4b1611d5c714f4a6fab401e5c85e2dd8
freejacque <freejacque@gmail.com>
2016-05-08 17:59:04 -0400

    added comments

--- kick-punch-block/functions.php
+++ kick-punch-block/functions.php
@@ -83,11 +83,11 @@ function kpb_scripts() {
   if(is_singular() && wp_attachment_is_image() ) {
     wp_enqueue_script( 'Keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
   }
-
+  // Register HTML5 shim
   wp_register_script('html5-shim', 'http://html5shim.googlecode.com/svn/trunk/html5.js', array( 'jquery' ), '1', false
 );
   wp_enqueue_script( 'html5-shim' );
-
+  // Register HTML5 OtherScript.js
   wp_register_script( 'custom-script', THEME_DIR . '/js_path/customscript.js', array( 'jquery' ), '1', false );
   wp_enqueue_script( 'custom-script' );
 }
