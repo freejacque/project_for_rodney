da4c02465b179552601faf13d0125d5ff4baff1e
freejacque <freejacque@gmail.com>
2016-05-08 18:37:07 -0400

    added link for favicon (need to replace path)

--- kick-punch-block/header.php
+++ kick-punch-block/header.php
@@ -41,6 +41,7 @@
   ?></title>
   <link rel="profile" href="http://gmpg.org/xfn/11" />
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+  <link rel="shortcut icon" href="<?php echo THEME_DIR; ?>/path/favicon.ico" /><!-- TODO replace favicon path -->
   <!--[if lt IE 9]>
   <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
   <![endif]-->
